e3ef92a17f9c19793ff745eaae46259a7b5f0065
hyfly233 <1274694743@qq.com>
2021-04-04 20:52:00 +0800

    Initial commit

---
+++ src/main/java/com/hyfly/milet/MiletApplication.java
@@ -0,0 +1,13 @@
+package com.hyfly.milet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiletApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyfly/milet/MiletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyfly.milet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
